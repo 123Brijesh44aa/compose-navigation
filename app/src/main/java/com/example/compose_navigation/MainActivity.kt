84b886d9a63ac04c9faa1ecee297d3d23e17a3d4
123Brijesh44aa <oddsworld0121@gmail.com>
2024-09-07 16:24:29 +0530

    new branch animate_composable_size created

--- app/src/main/java/com/example/compose_navigation/MainActivity.kt
+++ app/src/main/java/com/example/compose_navigation/MainActivity.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
-import com.example.compose_navigation.navigation.AppNavHost
-import com.example.compose_navigation.screens.MainScreen
 import com.example.compose_navigation.ui.theme.ComposeNavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,12 +20,7 @@ class MainActivity : ComponentActivity() {
             ComposeNavigationTheme {
                 val navController = rememberNavController()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    AppNavHost(
-//                        navHostController = navController,
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-
-                    MainScreen(navController = navController, modifier = Modifier.padding(innerPadding))
+                   Box(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
